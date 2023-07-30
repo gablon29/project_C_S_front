880d5054c17f077c7c5e0aa0e0f05f3223f4ec16
gabri <gabriellondero40@gmail.com>
2023-07-30 19:14:21 -0300

    desarrollando el form para el login

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 function App() {
   
 
   return (
-    <>
-    <h2></h2>
-    </>
+    <div>
+    <Form>
+      <Form.Group style={{margin: '10px'}} className="mb-3" controlId="formBasicEmail">
+        <Form.Label style={{margin: '20px'}}>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group style={{margin: '5px'}} className="mb-3" controlId="formBasicPassword">
+        <Form.Label style={{margin: '20px'}}>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </div>
   )
 }
 
