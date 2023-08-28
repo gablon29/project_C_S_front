b5867eae7e62fa55cca6d5c8f4c8c13f5b96d709
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-28 17:54:20 -0300

    creando el estado con react-redux

--- src/redux/userSlice.js
+++ src/redux/userSlice.js
@@ -1,14 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 import data from "../../data/data.json";
 
-const initialState = {
-  useradmin: [],
-};
+const initialState = data;
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
+    add_New_Table: (state, action) => {
+      const id = crypto.randomUUID();
+      const status = "active";
+      return [...state, { id, status, ...action.payload }];
+    },
     getUser: (state, action) => {
       state.useradmin = action.payload;
     },
