e4599a999db57590c3bb0320967a9be3f260c4fd
gabri <gabriellondero40@gmail.com>
2023-08-31 07:19:07 -0300

    incluyendo las props-types

--- src/Component/detailComp/DetailComp.jsx
+++ src/Component/detailComp/DetailComp.jsx
@@ -1,15 +1,19 @@
 import { Card, Title, Badge } from "@tremor/react";
+import PropTypes from "prop-types";
 
-function DetailComp(props) {
+const DetailComp = ({ name, last_name }) => {
   return (
     <Card>
       <Title>Detalles del Pastor</Title>
       <Card>
-        <Title>Nombre: {props.name}</Title>
-        <Title>Nombre: {props.last_name}</Title>
+        <Title>Nombre: {name}</Title>
+        <Title>Nombre: {last_name}</Title>
       </Card>
     </Card>
   );
-}
-
+};
+DetailComp.propTypes = {
+  name: PropTypes.string.isRequired,
+  last_name: PropTypes.string.isRequired,
+};
 export default DetailComp;
